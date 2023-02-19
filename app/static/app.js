f111f63a8970c14cac576ad0ae112c47cb9dd7e0
Joao Antonio <joaoantoniosampaio04@outlook.com>
2023-02-19 01:36:43 -0300

    fix: correções nos arquivos de html

--- app/static/app.js
+++ app/static/app.js
@@ -1,5 +1,6 @@
 function redirect() {
-    let button = document.getElementById("redirect_button");
-    let dist = button.getAttribute('name')
+    let button = document.getElementById("redirect-button");
+    console.log(button);
+    let dist = button.getAttribute('name');
     window.location.href=`/${dist}`;
 }
